6f5f703e2a3bd35056fead54c2e3bfd3b35ebce1
kevinrlutz <kevinlutz429@gmail.com>
2022-07-19 16:11:05 -0400

    Added search by last name feature

--- src/main/java/com/perficient/userservice/repositories/UserRepository.java
+++ src/main/java/com/perficient/userservice/repositories/UserRepository.java
@@ -1,17 +1,10 @@
 package com.perficient.userservice.repositories;
 
 import com.perficient.userservice.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.List;
 
 public interface UserRepository extends MongoRepository<User, String> {
-
+    List<User> findByLastNameIgnoreCase(String lastName);
 }
