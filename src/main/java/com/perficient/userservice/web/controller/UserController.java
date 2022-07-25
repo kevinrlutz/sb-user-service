2c950f73eee28977fefcfa2e59cbfb00218c9a8c
kevinrlutz <kevinlutz429@gmail.com>
2022-07-25 16:46:34 -0400

    UserDTO validation + Response statuses for controller

--- src/main/java/com/perficient/userservice/web/controller/UserController.java
+++ src/main/java/com/perficient/userservice/web/controller/UserController.java
@@ -20,31 +20,37 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<UserDto> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<UserDto> getUserById(@PathVariable("userId") String userId) {
         return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
     }
 
     @GetMapping("/search/{lastName}")
+    @ResponseStatus(HttpStatus.OK)
     public List<UserDto> getAllUsersByLastName(@PathVariable("lastName") String lastName) {
         return userService.getAllUsersByLastName(lastName);
     }
 
     @GetMapping("/{userId}/appointments")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<ApptDto>> getUserAppointments(@PathVariable("userId") String userId) {
         return new ResponseEntity<>(userService.getUserAppointments(userId), HttpStatus.OK);
     }
 
     @PostMapping("/") // Create a new user
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<UserDto> createUser(@RequestBody @Validated UserDto userDto) {
         return new ResponseEntity<>(userService.saveNewUser(userDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{userId}") // Update an existing user
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity updateUserById(@PathVariable("userId") String userId, @RequestBody @Validated UserDto userDto) {
         return new ResponseEntity<>(userService.updateUser(userId, userDto), HttpStatus.NO_CONTENT);
     }
