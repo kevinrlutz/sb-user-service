39c054427e25491ed52cfc384c5f05f45f19a129
Rayaan Attari <rayaan.attari@gmail.com>
2022-07-26 18:08:40 -0500

    Remove Eureka Dependency

--- src/main/java/com/perficient/userservice/UserServiceApplication.java
+++ src/main/java/com/perficient/userservice/UserServiceApplication.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
